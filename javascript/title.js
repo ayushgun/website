c2ee7109ce4ce1b3c3e5f42150dc9e6a44a41381
Ayush Gundawar <34173777+ayushgun@users.noreply.github.com>
2021-11-03 21:39:31 -0400

    Changed to randomize icon

---
+++ javascript/title.js
@@ -0,0 +1,45 @@
+/*
+* A modified JS file that allows for the 
+* typing effect in the title bar
+*/
+(function (_0x19e23a, _0x484016) {
+    var _0x2ac09f = _0x3670
+        , _0x1e3366 = _0x19e23a();
+    while (!![]) {
+        try {
+            var _0x4e9bd4 = -parseInt(_0x2ac09f(0x1cf)) / 0x1 * (-parseInt(_0x2ac09f(0x1d5)) / 0x2) + -parseInt(_0x2ac09f(0x1e2)) / 0x3 * (-parseInt(_0x2ac09f(0x1ce)) / 0x4) + parseInt(_0x2ac09f(0x1d2)) / 0x5 + -parseInt(_0x2ac09f(0x1db)) / 0x6 * (-parseInt(_0x2ac09f(0x1d7)) / 0x7) + -parseInt(_0x2ac09f(0x1d3)) / 0x8 + parseInt(_0x2ac09f(0x1de)) / 0x9 + parseInt(_0x2ac09f(0x1d8)) / 0xa * (-parseInt(_0x2ac09f(0x1d9)) / 0xb);
+            if (_0x4e9bd4 === _0x484016)
+                break;
+            else
+                _0x1e3366['push'](_0x1e3366['shift']());
+        } catch (_0x51d743) {
+            _0x1e3366['push'](_0x1e3366['shift']());
+        }
+    }
+}(_0x2210, 0xce175));
+var i = 0x0;
+function _0x3670(_0x25520a, _0x2b4b81) {
+    var _0x2210ae = _0x2210();
+    return _0x3670 = function (_0x367017, _0x1c059f) {
+        _0x367017 = _0x367017 - 0x1cb;
+        var _0x342766 = _0x2210ae[_0x367017];
+        return _0x342766;
+    }
+        ,
+        _0x3670(_0x25520a, _0x2b4b81);
+}
+setInterval(function () {
+    var _0x434dda = _0x3670
+        , _0x3c715a = ['Ayush Gundawar'];
+    i === _0x3c715a[_0x434dda(0x1d6)] && (i = 0x0),
+        document[_0x434dda(0x1d4)] = _0x3c715a[i],
+        i++;
+}, 0xc8);
+function _0x2210() {
+    var _0x347349 = ['21EMAHGP', '10YowFAL', '39709439SuYiGn', 'ayush.gundawar.me', '1596858iBqIpd', 'Ayush', '', '13473198OdeoRv', 'hecker\x20was\x20here', 'ayu', 'a522f', '2437599Nfhbyw', 'Ayus', 'cool-', 'Ayush', '4XpsiHN', '1515077eHSito', 'ayush.gundawar.me', 'console.log(ip)', '4799875pxfMME', '9031504rzfTHX', 'title', '2GpElmS', 'length'];
+    _0x2210 = function () {
+        return _0x347349;
+    }
+        ;
+    return _0x2210();
+}
