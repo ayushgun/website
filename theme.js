b8801c64b20b697ba97cec6c3d169d1600787a86
Ayush <ayushgundawar@gmail.com>
2022-11-08 18:25:21 -0600

    first commit

---
+++ theme.js
@@ -0,0 +1,12 @@
+function switchTheme() {
+	const body = document.body;
+	body.classList.toggle("light-mode");
+}
+
+function change(iconID) {
+	if (document.getElementById(iconID).className == "fa-regular fa-lightbulb") {
+		document.getElementById(iconID).className = "fa-solid fa-lightbulb";
+	} else {
+		document.getElementById(iconID).className = "fa-regular fa-lightbulb";
+	}
+}
